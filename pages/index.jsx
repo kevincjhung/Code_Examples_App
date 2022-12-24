3a2b0032a7630f5f84d70b6789930c90f2b64018
Kevin Hung <kevincjhung@gmail.com>
2022-12-23 19:26:28 -0800

    ..

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 import '../public/loading.svg'
 
 // import Skeleton from 'react-loading-skeleton'
-// import 'react-loading-skeleton/dist/skeleton.css'
+// import 'react-loading-skeleton/dist/skeleton.css' //
 
 import Head from 'next/head'
 
